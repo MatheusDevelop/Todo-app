4ed37a2e534d89439e31f90b1f39c24080ef1d45
Matheus Barbosa <mathvierabarbosa@gmail.com>
2020-09-08 12:46:44 -0300

    pomodoro finalizado

--- Frontend Angular/src/app/components/template/pomodoro/pomodoro.component.ts
+++ Frontend Angular/src/app/components/template/pomodoro/pomodoro.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-pomodoro',
@@ -10,38 +11,78 @@ export class PomodoroComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    
   }
 
   leftMinutes=25;
-
+  leftSecounds=60;
   // 0 = PLAY
   // 1 = STOP
   // 2 = REPEAT
-  buttonStates=0;
 
+  buttonStates =0;
 
-  clear(func){
-    return 
-  }
+  stop=false;
+  
   
   pomodoroClock(){
     
-    let x = setInterval(()=>{
-      if(this.leftMinutes>0){
-        this.leftMinutes=this.leftMinutes-1; 
+    let secounds = setInterval(()=>{
+      if(this.leftSecounds>0){
+
+        if(this.stop){                    
+          clearInterval(secounds);           
+          this.stop=false;         
+
+        }else{
+          this.leftSecounds--;
+          
+        }
       }else{
-        this.buttonStates=2;
-        //Fazer uma notificação pra quando terminar o pomodoro
-        alert('Pomodoro terminado');
-        clearInterval(x);
+
+        this.leftMinutes--
+
+        if(this.leftMinutes>0){
+          this.leftSecounds=60;
+        }else{
+          //seta para um state que oculta os botoes e so mostra a opçao de reiniciar o clock
+          this.buttonStates=2;
+
+          this.playAudio()
+          clearInterval(secounds);
+        }
       }
-    },6000)
+    },1000)  
+    
 
+    
+    
     this.buttonStates=1;
-   
-   
+  }
 
 
+  playAudio(){
+    let audio = new Audio();
+    audio.src = "../../../assets/audio/notification.mp3";
+    audio.load();
+    audio.play();
+  }
+
+
+  stopClock(){
+    this.stop=true;
+    this.buttonStates=0;
+    
+  }
+    
+  restoreClock(){
+    
+    this.leftMinutes=24;
+    this.leftSecounds=60;
+    this.buttonStates=0;
+    
+    
+    
   }
 
 }
